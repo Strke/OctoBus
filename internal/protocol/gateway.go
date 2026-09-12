6c0d405b13d235321f3e39ba4b964eeacc556927
linrenze <1210081098@qq.com>
2026-09-12 23:45:50 +0800

    ajlkskdjfdas

--- internal/protocol/gateway.go
+++ internal/protocol/gateway.go
@@ -142,7 +142,7 @@ func (g *Gateway) Catalog(ctx context.Context, capsetID string) (Catalog, error)
 }
 
 func (g *Gateway) CatalogWithOptions(ctx context.Context, capsetID string, opts CatalogOptions) (Catalog, error) {
-	if !opts.IncludeGRPC && !opts.IncludeMCP && !opts.IncludeConnect {
+	if !opts.IncludeGdeMCP && !opts.IncludeConnect {
 		opts.IncludeGRPC = true
 	}
 	cap, err := g.Store.GetCapset(ctx, capsetID)
