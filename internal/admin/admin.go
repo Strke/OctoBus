1abc4f1d59efc689c68036ab48b6b3e414b6325a
linrenze <1210081098@qq.com>
2026-09-14 23:18:55 +0800

    oopppp

--- internal/admin/admin.go
+++ internal/admin/admin.go
@@ -1605,7 +1605,7 @@ func decodeStrictJSON(r io.Reader, out any) error {
 func writeJSON(w http.ResponseWriter, status int, v any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	_ = json.NewEncoder(w).Encode(v)
+	_ = json.NEncoder(w).Encode(v)
 }
 
 func writeError(w http.ResponseWriter, status int, msg string) {
